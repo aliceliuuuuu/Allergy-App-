e28de6758c60231dbe5249c99579757587060d46
aliceliuuuuu <aliceliu061020@sina.com>
2023-09-23 22:34:17 +0800

    first commit

---
+++ lib/account screen.dart
@@ -0,0 +1,73 @@
+import "package:flutter/material.dart";
+class UserInfoPage extends StatelessWidget {
+  @override
+  Widget build( BuildContext context ) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("YOUR INformatioN"),
+      ),
+      body: UserInfoForm(),
+    );
+
+  }
+
+}
+class UserInfoForm extends StatefulWidget {
+  @override
+  _UserInfoFormState createState( ) => _UserInfoFormState( );
+
+}
+class _UserInfoFormState extends State<UserInfoForm> {
+  @override
+  Widget build( BuildContext context ) {
+    return Center(
+      child: Padding(
+        padding: EdgeInsets.all(50),
+        child: ListView (
+          children: [Column(
+              //mainAxisAlignment: MainAxisAlignment.center,
+              children:<Widget>[
+                TextField(
+                  decoration: InputDecoration(labelText:"name:"),
+                ),
+                SizedBox(height: 20),
+                TextField(
+                  decoration: InputDecoration(labelText:"age:"),
+                ),
+                SizedBox(height: 20),
+                TextField(
+                  decoration: InputDecoration(labelText:"season and month"),
+                ),
+                SizedBox(height: 20),
+                TextField(
+                  decoration: InputDecoration(labelText:"location"),
+                ),
+                SizedBox(height: 20),
+                TextField(
+                  decoration: InputDecoration(labelText:"asthma:"),
+                ),
+                SizedBox(height: 20),
+                TextField(
+                  decoration: InputDecoration(labelText:"smoking:"),
+                ),
+                SizedBox(height: 20),
+                TextButton(
+                  child:Text("save"),
+                  onPressed: (){
+
+                  },
+                )
+              ]
+          ),]
+        )
+
+      )
+
+
+
+
+
+    );
+
+  }
+}
